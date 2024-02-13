122167eb78c89969052dfc7fb7f9f8aac96ee962
Sherzotbek Yuldashev <147944821+YuldashevSherzotbek@users.noreply.github.com>
2024-02-13 12:48:51 +0300

    Delete 4 задача питон main.py

--- 4 задача питон main.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(number):
-    return str(number) == str(number)[::-1]
-
-number = int(input("Введите число: "))
-
-if is_palindrome(number):
-    print("Число является палиндромом.")
-else:
-    print("Число не является палиндромом.")
